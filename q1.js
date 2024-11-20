64d460e0e4ce7c54b8e7bb97ce66220cbade0a9a
CENextGen <cenextgen2024@gmail.com>
2024-11-20 21:18:15 +0700

    add question

---
+++ q1.js
@@ -0,0 +1,13 @@
+let num = 5;
+
+if (num > 5) {
+    num = num - 5;
+} else {
+    num = num + 2;
+}
+
+if (num > 2) {
+    console.log(num - 1);
+} else {
+    console.log(num + 5);
+}
---
+++ q2.js
@@ -0,0 +1,6 @@
+function func1(num) {
+    console.log("Hello CE NextGen");
+}
+
+const result = func1(15);
+console.log(result);
